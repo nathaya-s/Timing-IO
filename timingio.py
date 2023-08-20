e382b54ec2af753cdf1194c8c13bfb543346d879
Nathaya-S <nathaya.ssn@gmail.com>
2023-08-20 12:50:30 +0700

    Timing I/O

--- timingio.py
+++ timingio.py
@@ -3,6 +3,7 @@ import os
 import csv
 import random
 
+
 def writefile(data):
     with open(completeName, "w", newline="") as csvfile:
         wt = csv.writer(csvfile)
@@ -11,21 +12,25 @@ def writefile(data):
         wt.writerows(data)
     csvfile.close()
 
+
 def openfile():
     f = open(completeName, "r")
     f.read()
+
 
 def cal_defect_ratio(x):
     sum_defect_ratio = 0
     for i in x:
         sum_defect_ratio += i[3]
     print("defect ratio average : ", sum_defect_ratio / len(x))
-    
+
+
 def cal_defect_box_ratio(x):
     sum_defect_box_ratio = 0
     for i in x:
         sum_defect_box_ratio += i[4]
     print("defect ratio average : ", sum_defect_box_ratio / len(x))
+
 
 start = time.time()
 save_path = './Timing-IO'
@@ -35,8 +40,8 @@ data = []
 for i in range(0, 50000):
     rand_1 = random.uniform(0.5, 1)
     rand_2 = random.randint(0, 1)
-    data.append(["009", "08/11/2563", "CBR1", rand_1, rand_2])
-    
+    data.append([i, "08/11/2563", "CBR1", rand_1, rand_2])
+
 writefile(data)
 openfile()
 
